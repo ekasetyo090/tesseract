729fd0cb386e2d87ad504a53987130a12aff179f
ekasetyo090 <ekaagung090@gmail.com>
2024-07-03 22:52:01 +0700

    add files

--- main project/pages/2_🤖_Content View Percentages Prediction.py
+++ main project/pages/2_🤖_Content View Percentages Prediction.py
@@ -11,14 +11,14 @@ import streamlit as st
 
 from YT_Scrapy import YtScraper
 from data_construct import data_constuct
-
+import time
 import concurrent.futures
 
 @st.cache_data
 def process_data(context):
     # Lakukan operasi yang ingin Anda jalankan secara bersamaan di sini
     # (misalnya, pemrosesan sub_topic atau primary_topic)
-    processed_context = ','.join(list(set((','.join(sub_topic_context)).split(','))))
+    processed_context = ','.join(list(set((','.join(context)).split(','))))
     return processed_context.lower()
 
 
@@ -27,39 +27,41 @@ obj_scraper = YtScraper()
 obj_construct = data_constuct()
 
 st.set_page_config(
-    page_title='🤖 Prediction Network')
-col1,col2 = st.columns(2)
+    page_title='🤖 TSS B')
+with st.spinner('Wait for it...'):
+    time.sleep(5)
+st.success('Model Under Development')
 
-with col1:
-    title = st.text_input("Content Title", '')
-with col2:                        
-    thumbnail = st.file_uploader(label='Upload Thumbnail', 
-                     type= ['png', 'jpg'], 
-                     accept_multiple_files=False, 
-                     key=None, 
-                     help=None, 
-                     on_change=None, 
-                     args=None, 
-                     disabled=False, 
-                     label_visibility="visible")
-preddict_prepare = False
-if title:
-    search_context = obj_scraper.scrape_search_video(q=title,max_data=100)
-    search_context = obj_construct.construct_df_ML(search_context)
+#with col1:
+    #title = st.text_input("Content Title", '')
+#with col2:                        
+    #thumbnail = st.file_uploader(label='Upload Thumbnail', 
+                     #type= ['png', 'jpg'], 
+                     #accept_multiple_files=False, 
+                     #key=None, 
+                     #help=None, 
+                     #on_change=None, 
+                     #args=None, 
+                     #disabled=False, 
+                     #label_visibility="visible")
+#preddict_prepare = False
+#if title:
+    #search_context = obj_scraper.scrape_search_video(q=title,max_data=100)
+    #search_context = obj_construct.construct_df_ML(search_context)
     #search_context = search_context[['sub-topic','primary_topic']]
-    sub_topic_context, primary_topic_context = (search_context['video_topic'].dropna().to_list(), 
-                                                search_context['topic_primary'].dropna().to_list())
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        future_sub_topic = executor.submit(process_data, sub_topic_context)
-        future_primary_topic = executor.submit(process_data, primary_topic_context)
+    #sub_topic_context, primary_topic_context = (search_context['video_topic'].dropna().to_list(), 
+                                                #search_context['topic_primary'].dropna().to_list())
+    #with concurrent.futures.ThreadPoolExecutor() as executor:
+        #future_sub_topic = executor.submit(process_data, sub_topic_context)
+        #future_primary_topic = executor.submit(process_data, primary_topic_context)
     
-        processed_sub_topic = future_sub_topic.result()
-        processed_primary_topic = future_primary_topic.result()
-    llm_input = f'''
-    title : {title} [SEP] 
-    primary topic : {processed_primary_topic} [SEP] 
-    sub topic : {processed_primary_topic}'''
-    preddict_prepare = True
+        #processed_sub_topic = future_sub_topic.result()
+        #processed_primary_topic = future_primary_topic.result()
+    #llm_input = f'''
+    #title : {title} [SEP] 
+    #primary topic : {processed_primary_topic} [SEP] 
+    #sub topic : {processed_primary_topic}'''
+    #preddict_prepare = True
     
 
     
