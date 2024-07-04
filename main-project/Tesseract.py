be7ffa87863f6a39e44bba0cb6147df74aec9247
ekasetyo090 <ekaagung090@gmail.com>
2024-07-05 01:06:45 +0700

    '.'

--- main-project/Tesseract.py
+++ main-project/Tesseract.py
@@ -68,7 +68,7 @@ with st.sidebar:
     else:
         df_filter = df_video.copy()
     df_filter.sort_values(by='hour', ascending=True,inplace=True)
-    list_day_filter = df_filter['day'].unique().append('All')
+    list_day_filter = list(df_filter['day'].unique()).append('All')
     option_day = st.selectbox("Content By Day",
                                     list_day_filter,index=-1)
     if option_day == 'All':
